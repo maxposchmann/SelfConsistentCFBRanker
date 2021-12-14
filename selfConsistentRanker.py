71fefb0f9a8e7c8d90fdefdeab79da202ffa5b70
maxposchmann <maxposchmann@gmail.com>
2021-12-14 11:41:09 -0500

    Army/Navy and bowl schedule update

--- selfConsistentRanker.py
+++ selfConsistentRanker.py
@@ -23,8 +23,8 @@ extendedPrint = True
 
 maxIts = 10000
 tol = 1e-14
-maxWeek = 15
-maxWeekRemaining = 16
+maxWeek = 16
+maxWeekRemaining = 20
 
 rankstrings = [(f'({i+1}) ') for i in range(25)]
 nameSwaps = [['Central Florida','UCF'],['Pittsburgh','Pitt'],['Alabama-Birmingham','UAB'],['Texas-San Antonio','UTSA'],
